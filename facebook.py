d359878db7cd2c5b77ba96b1dc74ae37a36f3a3d
apichardo2v <anglp2002@uri.edu>
2023-03-27 19:29:00 -0400

    added facebook code

---
+++ facebook.py
@@ -0,0 +1,8 @@
+userName = input("Hello! Please enter your facebook username! ")
+
+userPassword = input("Hello! Please enter your facebook password! ")
+
+if(userName.lower() == "angel" and userPassword.lower() == "programmingrocks!"):
+    print("Welcome to facebook!")
+else:
+    print("Wrong credentials, try again!")
